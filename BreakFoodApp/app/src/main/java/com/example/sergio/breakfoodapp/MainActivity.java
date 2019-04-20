0ceb0149be339c3910f8e34d9bd6e9d0ac5e3cbf
Eduardo Jirón <emjironal@gmail.com>
2019-04-20 16:22:55 -0600

    Ejemplos conexion backend

--- BreakFoodApp/app/src/main/java/com/example/sergio/breakfoodapp/MainActivity.java
+++ BreakFoodApp/app/src/main/java/com/example/sergio/breakfoodapp/MainActivity.java
@@ -64,12 +64,12 @@ public class MainActivity extends AppCompatActivity {
         txtRecuperar=(TextView)findViewById(R.id.txtInicioRecuperar);
         txtRecuperar.setOnClickListener(recuperar);
 
-        String url = "http://192.168.0.10:8000/android/login";
+        String url = "http://192.168.0.10:8000/android/login";//cambiar a https://appetyte.herokuapp.com/
         List<NameValuePair> user = new ArrayList<>();
         user.add(new BasicNameValuePair("username", "mauri"));
         user.add(new BasicNameValuePair("password", "1234"));
         Log.e("Error", LectorHttpResponse.leer(GestorPostRequest.postData(url, user)));
-        url = "http://192.168.0.10:8000/android/";
+        url = "http://192.168.0.10:8000/android/getRestaurantes";
         Log.e("Error", LectorHttpResponse.leer(GestorGetRequest.getData(url)));
     }
 
