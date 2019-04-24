5d9f1fe1a4a3a3ae733fb329ce4ef0705802b5ec
Jouse <josuecanalesmena@gmail.com>
2019-04-23 18:07:39 -0600

    Implementacion con Mixpanel

--- BreakFoodApp/app/src/main/java/com/example/sergio/breakfoodapp/ChangePasswordActivity.java
+++ BreakFoodApp/app/src/main/java/com/example/sergio/breakfoodapp/ChangePasswordActivity.java
@@ -2,6 +2,8 @@ package com.example.sergio.breakfoodapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class ChangePasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+    }
+
+
+
+
+    public void changePasswors(View view) {
+
+        String password = ((EditText)findViewById(R.id.change_password_password_edittext)).getText().toString();
+        String code =  ((EditText)findViewById(R.id.change_password_code_edittext)).getText().toString();
+
+
+
     }
 }
